7880e48838b9202543f4924c818c91d42f43c0d6
hamburml <real.hamburml@gmail.com>
2022-09-11 14:33:12 +0200

    added tests

---
+++ dynamodb-enhanced-brokentest/src/main/java/org/acme/Table.java
@@ -0,0 +1,29 @@
+package org.acme;
+
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbPartitionKey;
+
+@DynamoDbBean
+public class Table {
+
+    private String id;
+
+    private String value;
+
+    @DynamoDbPartitionKey
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
---
+++ dynamodb-enhanced-test/src/main/java/org/acme/GreetingResource.java
@@ -0,0 +1,40 @@
+package org.acme;
+
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
+import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class GreetingResource {
+
+    @Inject
+    DynamoDbClient clientOld;
+    @Inject
+    DynamoDbEnhancedClient client;
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        TableSchema<Table> schema = TableSchema.fromClass(Table.class);
+
+        DynamoDbTable<Table> myTable = client.table("myTable", schema);
+        myTable.createTable();
+
+        Table item = new Table();
+        item.setId("asdf");
+        item.setValue("VALUE");
+        myTable.putItem(item);
+        myTable.deleteTable();
+
+
+
+        return "Hello RESTEasy";
+    }
+}
